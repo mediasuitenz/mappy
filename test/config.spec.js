3df045cc4ed5a7587707736ccd5eff5209417115
Mark Stuart <mstuart_is@me.com>
2014-09-09 17:09:48 +1200

    Moved config test into specific file
    Removed rewire dependency for this test in the interim

---
+++ test/config.spec.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/*
+config
+->read from file, holds values and supplies as needed
+*/
+var should = require('chai').should()
+
+var config = require('../lib/config')()
+
+describe('Config', () => {
+  describe('#getLayers', () => {
+    it('should return layers config array', () => {
+      //Given
+
+      //When
+      var layers = config.getLayers()
+
+      //Then
+      layers.should.be.an('array')
+      layers[0].name.should.be.a('string')
+    })
+  })
+})
