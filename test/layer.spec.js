bf8fbfdcb7f32fd1a7d1fd033a4a40cf5e496880
Mark Stuart <mstuart_is@me.com>
2014-09-10 14:18:01 +1200

    Add layer test

---
+++ test/layer.spec.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+layer
+- has own data service
+- has config
+- insert item in key
+- style up items
+- inject styled content into popup
+*/
+require('chai').should()
+
+describe('Layer', () => {
+
+})
