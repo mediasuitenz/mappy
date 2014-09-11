2a61e06a3716d0c381b31dc99b52423afee49819
Mark Stuart <mstuart_is@me.com>
2014-09-11 21:51:17 +1200

    Add tests and basic outline of Layer

--- test/layer.spec.js
+++ test/layer.spec.js
@@ -7,9 +7,40 @@ layer
 - insert item in key
 - style up items
 - inject styled content into popup
+- listens to it's dataService for updated geojson content
+- can return it's latest geojson
 */
 require('chai').should()
 
+var layerFactory = require('../lib/layer')
+
 describe('Layer', () => {
+  //Given
+  var layer = layerFactory({})
+
+  describe('#getData', () => {
+    it('should return its latest geojson', () => {
+      //When
+      var geojson = layer.getData()
+
+      //Then
+      geojson.should.be.an('object')
+    })
+  })
+
+
+
+  describe('#onData', () => {
+    it('should be emitted when dataService onData event received', () => {
+      //Given
+      //dataService and layer
+
+      //When
+      //dataService emits a data ready event
+
+      //Then
+      //layer should emit onData event
+    })
+  })
 
 })
