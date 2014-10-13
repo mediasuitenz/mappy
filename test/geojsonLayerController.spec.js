8cb6b7b2e833b1d4c42827f2e66a5bce5c44a417
Mark Stuart <mstuart_is@me.com>
2014-10-13 21:49:54 +1300

    Add basic test for geojsonLayerController

---
+++ test/geojsonLayerController.spec.js
@@ -0,0 +1,50 @@
+'use strict';
+
+require('mocha-given')
+var chai       = require('chai')
+var expect     = chai.expect
+var context    = describe
+var catchError = require('catch-error')
+var sinon      = require('sinon')
+var sinonChai  = require('sinon-chai')
+chai.use(sinonChai)
+
+var gjLayerControllerFactory = require('../lib/geojsonLayerController')
+
+class Map {}
+class KeyController {}
+var dataServiceFactory = () => {
+  return {}
+}
+var popupPresenterFactory = () => {
+  return {}
+}
+var stylePresenterFactory = () => {
+  return {}
+}
+var filterFactory = () => {
+  return {}
+}
+
+describe('geojsonLayerController', () => {
+  describe('creating a geojsonLayerController', () => {
+    var config, gjLayerController
+
+    Given('some config', () => {
+      config = {
+        map: new Map(),
+        keyController: new KeyController(),
+        dataServiceFactory: dataServiceFactory,
+        popupPresenterFactory: popupPresenterFactory,
+        stylePresenterFactory: stylePresenterFactory,
+        filterFactory: filterFactory
+      }
+    })
+    When('the module is called with config', () => {
+      gjLayerController = gjLayerControllerFactory(config)
+    })
+    Then('the module should be an object', () => {
+      expect(gjLayerController).to.be.an('object')
+    })
+  })
+})
