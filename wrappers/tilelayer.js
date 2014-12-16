6381caa62c50bce939a14a819345ea2d39fb66e0
Richard Walker <digitalsadhu@gmail.com>
2014-12-16 22:46:13 +1300

    Fix jshint errors

--- wrappers/tilelayer.js
+++ wrappers/tilelayer.js
@@ -5,14 +5,14 @@ require('../vendor/leaflet-plugins-google')
 
 /**
  * Intermediary tile layer used to generate either a Google layer, or a Leaflet tileLayer.
- * 
+ *
  * @param {string} name
  * @param {object} config
  */
 module.exports = function (name, config) {
   var tileLayer = null
 
-  if(config.type == 'google') {
+  if(config.type === 'google') {
     tileLayer = new L.Google((config.mapType ? config.mapType : 'ROADMAP'))
   } else {
     tileLayer = L.tileLayer(config.url, config)
